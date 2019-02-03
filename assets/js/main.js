0ccb0e64ae95ae61551cf5a7d811e779391ce801
Anna-Maria Hartner <sunshine4330@gmail.com>
2019-02-03 11:04:12 -0500

    Datenschutzerklärung

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,7 +171,15 @@
 					$image.hide();
 
 			});
-			
-			
+	//Cookie Consent
+	$(document).ready(function(){
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(200);
+     }, 4000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    });
+});
+
 
 })(jQuery);
